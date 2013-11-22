17f27148189e299279c9a8c338f531fdc6b15709
Murilo Pontes <murilopontes@gmail.com>
2013-11-22 02:10:34 -0300

    xbox360 gamepad support in openflight

---
+++ pc/OpenFlightGamepad/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace OpenFlight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_send_count = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel_recv_count = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart7 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(409, 107);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(370, 97);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_send_count,
+            this.toolStripStatusLabel_recv_count});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 668);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(801, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_send_count
+            // 
+            this.toolStripStatusLabel_send_count.Name = "toolStripStatusLabel_send_count";
+            this.toolStripStatusLabel_send_count.Size = new System.Drawing.Size(72, 17);
+            this.toolStripStatusLabel_send_count.Text = "UDP send=0";
+            // 
+            // toolStripStatusLabel_recv_count
+            // 
+            this.toolStripStatusLabel_recv_count.Name = "toolStripStatusLabel_recv_count";
+            this.toolStripStatusLabel_recv_count.Size = new System.Drawing.Size(69, 17);
+            this.toolStripStatusLabel_recv_count.Text = "UDP recv=0";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(409, 210);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(370, 114);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(409, 440);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(370, 104);
+            this.chart3.TabIndex = 3;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(409, 550);
+            this.chart4.Name = "chart4";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart4.Series.Add(series4);
+            this.chart4.Size = new System.Drawing.Size(370, 115);
+            this.chart4.TabIndex = 4;
+            this.chart4.Text = "chart4";
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart5.Legends.Add(legend5);
+            this.chart5.Location = new System.Drawing.Point(409, 330);
+            this.chart5.Name = "chart5";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart5.Series.Add(series5);
+            this.chart5.Size = new System.Drawing.Size(370, 104);
+            this.chart5.TabIndex = 5;
+            this.chart5.Text = "chart5";
+            // 
+            // chart6
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart6.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart6.Legends.Add(legend6);
+            this.chart6.Location = new System.Drawing.Point(12, 369);
+            this.chart6.Name = "chart6";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart6.Series.Add(series6);
+            this.chart6.Size = new System.Drawing.Size(370, 114);
+            this.chart6.TabIndex = 6;
+            this.chart6.Text = "chart6";
+            // 
+            // chart7
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart7.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart7.Legends.Add(legend7);
+            this.chart7.Location = new System.Drawing.Point(12, 249);
+            this.chart7.Name = "chart7";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart7.Series.Add(series7);
+            this.chart7.Size = new System.Drawing.Size(370, 114);
+            this.chart7.TabIndex = 7;
+            this.chart7.Text = "chart7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 690);
+            this.Controls.Add(this.chart7);
+            this.Controls.Add(this.chart6);
+            this.Controls.Add(this.chart5);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "OpenFlight Gamepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_send_count;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_recv_count;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart7;
+    }
+}
+
---
+++ pc/OpenFlightGamepad/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using XInputDotNetPure;
+
+namespace OpenFlight
+{
+    public partial class Form1 : Form
+    {
+        private Drone drone = new Drone();
+        private UdpLogger log;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            log = new UdpLogger(7778, string.Format("{0:yyyyMMddHHmmss}_navlog.csv", DateTime.Now));
+            timer1.Interval = 1000/10;
+            timer1.Enabled = true;
+
+            
+            this.chart1.Series.Clear();
+            this.chart2.Series.Clear();
+            this.chart3.Series.Clear();
+            this.chart4.Series.Clear();
+            this.chart5.Series.Clear();
+            this.chart6.Series.Clear();
+            this.chart7.Series.Clear();
+
+            this.chart1.Series.Add(log_ax);
+            this.chart1.Series.Add(log_ay);
+            this.chart1.Series.Add(log_az);
+
+            this.chart2.Series.Add(log_gx);
+            this.chart2.Series.Add(log_gy);
+            this.chart2.Series.Add(log_gz);
+
+            this.chart5.Series.Add(log_act_h);
+            this.chart5.Series.Add(log_set_h);
+            this.chart5.Series.Add(log_act_hv);
+
+            this.chart3.Series.Add(log_act_pitch);
+            this.chart3.Series.Add(log_act_roll);
+            this.chart3.Series.Add(log_act_yaw);
+
+            this.chart3.Series.Add(log_set_pitch);
+            this.chart3.Series.Add(log_set_roll);
+            this.chart3.Series.Add(log_set_yaw);
+
+            this.chart4.Series.Add(log_throttle);
+            this.chart4.Series.Add(log_adj_pitch);
+            this.chart4.Series.Add(log_adj_roll);
+            this.chart4.Series.Add(log_adj_yaw);
+
+            this.chart7.Series.Add(udp_tx_h);
+            this.chart6.Series.Add(udp_tx_pitch);
+            this.chart6.Series.Add(udp_tx_roll);
+            this.chart6.Series.Add(udp_tx_yaw);
+
+            foreach (Series x in chart1.Series) {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart2.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart3.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart4.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart5.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart6.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+            foreach (Series x in chart7.Series)
+            {
+                x.ChartType = SeriesChartType.FastLine;
+            }
+        }
+
+
+        Series log_ax = new Series("ax m/s^2 (front/rear)");
+        Series log_ay = new Series("ay m/s^2 (left/right)");
+        Series log_az = new Series("az m/s^2 (top/bottom)");
+
+        Series log_gx = new Series("gx (left/right)");
+        Series log_gy = new Series("gy (front/rear)");
+        Series log_gz = new Series("gz");
+
+        Series log_act_h = new Series("act_h (cm)");
+        Series log_act_pitch = new Series("act_pitch (graus)");
+        Series log_act_roll = new Series("act_roll (graus)");
+        Series log_act_yaw = new Series("act_yaw (graus)");
+        Series log_act_hv = new Series("act_hv");
+
+
+        Series log_set_h = new Series("set_h (cm)");
+        Series log_set_pitch = new Series("set_pitch (graus)");
+        Series log_set_roll = new Series("set_roll (graus)");
+        Series log_set_yaw = new Series("set_yaw (graus)");
+
+        Series log_throttle = new Series("throttle");
+        Series log_adj_pitch = new Series("adj_pitch (throttle)");
+        Series log_adj_roll = new Series("adj_roll (throttle)");
+        Series log_adj_yaw = new Series("adj_yaw (throttle)");
+
+        Series udp_tx_pitch = new Series("tx_pitch (graus [-5,5])");
+        Series udp_tx_roll = new Series("tx_roll (graus [-5,5])");
+        Series udp_tx_yaw = new Series("tx_yaw (graus [-5,5])");
+        Series udp_tx_h = new Series("tx_h (cm [0-600])");
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.toolStripStatusLabel_recv_count.Text = "UDP RX " + log.msgcount + " packets";
+
+            if (log.msgcount > 0)
+            {
+                //acelerometro
+                log_ax.Points.AddY(log.ax / 100000);
+                log_ay.Points.AddY(log.ay / 100000);
+                log_az.Points.AddY(log.az / 100000);
+
+                //giroscopio
+                log_gx.Points.AddY(log.gx / 100000);
+                log_gy.Points.AddY(log.gy / 100000);
+                log_gz.Points.AddY(log.gz / 100000);
+
+                //
+                log_act_pitch.Points.AddY(log.act_pitch / 100000);
+                log_act_roll.Points.AddY(log.act_roll / 100000);
+                log_act_yaw.Points.AddY(log.act_yaw / 100000);
+
+                //
+                log_act_h.Points.AddY(log.act_h / 100000);
+                log_set_h.Points.AddY(log.set_h / 100000);
+                log_act_hv.Points.AddY(log.act_hv / 100000);
+
+                log_set_pitch.Points.AddY(log.set_pitch / 100000);
+                log_set_roll.Points.AddY(log.set_roll / 100000);
+                log_set_yaw.Points.AddY(log.set_yaw / 100000);
+
+                //motores
+                log_throttle.Points.AddY(log.throttle / 100000);
+                log_adj_pitch.Points.AddY(log.throttle_pitch_adj / 100000);
+                log_adj_roll.Points.AddY(log.throttle_roll_adj / 100000);
+                log_adj_yaw.Points.AddY(log.throttle_yaw_adj / 100000);
+            }
+
+
+            //xbox360 gamepad with circular deadzone analog noise reduction
+            GamePadState state = GamePad.GetState(PlayerIndex.One, GamePadDeadZone.Circular);
+
+            /////////////////////////////////////////////////////////
+            //HOVER (planar: por padrao fica parado no mesmo lugar)
+            drone.Pitch = 0;
+            drone.Roll = 0;
+            drone.Yaw = 0;
+            //////////////////////////////////////////////////////////////////////////////////////
+            //PITCH (passo para frente e para tras)
+            drone.Pitch += DEG2RAD(state.ThumbSticks.Left.Y * 5);
+            ///////////////////////////////////////////////////////////////////////////////////////
+            //ROLL (andar de lado para esquerda e direta)
+            drone.Roll += DEG2RAD(state.ThumbSticks.Left.X * 5);
+            ///////////////////////////////////////////////////////////////////////////////////////
+            //Throttle (subir e descer)
+            drone.H += state.ThumbSticks.Right.Y*5;
+            //Yaw (Rotate clockwise/anticlockwise)
+            drone.Yaw += DEG2RAD(state.ThumbSticks.Right.X*5);
+
+            if (state.Buttons.A == XInputDotNetPure.ButtonState.Pressed)
+            {
+                //takeoff
+                drone.Pitch = 0;
+                drone.Roll = 0;
+                drone.Yaw = 0;
+                drone.H = 25;
+            }
+            if (state.Buttons.B == XInputDotNetPure.ButtonState.Pressed)
+            {
+                //land / emergency
+                drone.Pitch = 0;
+                drone.Roll = 0;
+                drone.Yaw = 0;
+                drone.H = 0;
+            }
+
+            udp_tx_h.Points.AddY(drone.H);
+            udp_tx_pitch.Points.AddY(RAD2DEG(drone.Pitch));
+            udp_tx_roll.Points.AddY(RAD2DEG(drone.Roll));
+            udp_tx_yaw.Points.AddY(RAD2DEG(drone.Yaw));
+
+            drone.SendCmd();
+            //////////////////////////////////////////////////////////////////////////////////////
+            
+            toolStripStatusLabel_send_count.Text = "UDP TX " + drone.cmd_count + " packets";
+
+
+
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        //degrees to radians
+        private double DEG2RAD(double x)
+        {
+            return x * 3.1415926 / 180;
+        }
+        //radians to degrees
+        private double RAD2DEG(double x)
+        {
+            return x / 3.1415926 * 180;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
