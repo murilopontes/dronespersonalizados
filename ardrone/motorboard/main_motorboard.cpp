2daedd21b26f938d335d29e28d9cd90a5320514e
Murilo Pontes <murilopontes@gmail.com>
2013-12-03 23:45:23 -0300

    new commands in motorboard for pitch, roll, yaw

--- ardrone/motorboard/main_motorboard.cpp
+++ ardrone/motorboard/main_motorboard.cpp
@@ -29,19 +29,38 @@
 #include "../open_ardrone_v1_lib/type.h"
 #include "../open_ardrone_v1_lib/util.h"
 #include "../open_ardrone_v1_lib/mot.h"
+#include "../open_ardrone_v1_lib/singleton.h"
 
 
-int main()
+int main(int argc, char** argv)
 {
+	//
+	singleton(argv[0]);
+
+	//
+	system("killall -9 program.elf");
+
 	printf("q=quit\r\n");
-	printf("<space>=stop\r\n");
-	printf(",=speed up\r\n");
-	printf(".=speed down\r\n");
-	printf("1=motor1 \r\n");
-	printf("2=motor2 \r\n");
-	printf("3=motor3 \r\n");
-	printf("4=motor4 \r\n");
-	printf("5=motor all \r\n");
+
+	printf("w=pitch front\r\n");
+	printf("e=pitch back\r\n");
+
+	printf("r=roll left\r\n");
+	printf("t=roll right\r\n");
+
+	printf("y=yaw cw\r\n");
+	printf("u=yaw ccw\r\n");
+
+	printf("<space>=stop all\r\n");
+	printf(",=speed up all\r\n");
+	printf(".=speed down all\r\n");
+
+	printf("1=motor1 min\r\n");
+	printf("2=motor2 min\r\n");
+	printf("3=motor3 min\r\n");
+	printf("4=motor4 min\r\n");
+	printf("5=motor all min \r\n");
+
 	printf("Leds:  a=loff\r\n");
 	printf("Leds:  s=green\r\n");
 	printf("Leds:  d=orange\r\n");
@@ -49,23 +68,76 @@ int main()
 
 	mot_Init();
 
+
 	float throttle1 = 0;
 	float throttle2 = 0;
 	float throttle3 = 0;
 	float throttle4 = 0;
-	float step=0.01;
+
+	//0.01-0.02 random motor shutdown
+	//
+	float step=0.03;
+    float min=0.03;
 
 	//main loop
 	while(1) {
 		//handle user input
 		int c=tolower(util_getch());
 
+		if(c=='w'){
+			printf("go front\r\n");
+			throttle1 = min; 	throttle2 = min;
+			throttle4 +=step;  throttle3 +=step;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
+		if(c=='e'){
+			printf("go back\r\n");
+			throttle1 +=step; 	throttle2 +=step;
+			throttle4 =min;  throttle3 =min;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
 
-		if(c=='q') break;
+		if(c=='r'){
+			printf("go left\r\n");
+			throttle1 +=step;  throttle2 =min;
+			throttle4 +=step;  throttle3 =min;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
+		if(c=='t'){
+			printf("go right\r\n");
+			throttle1 =min; 	throttle2 +=step;
+			throttle4 =min;  throttle3 +=step;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
+
+		if(c=='y'){
+			printf("go cw\r\n");
+			throttle1 =min;  throttle2 +=step;
+			throttle4 +=step;  throttle3 =min;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
+		if(c=='u'){
+			printf("go ccw\r\n");
+			throttle1 +=step; 	throttle2 =min;
+			throttle4 =min;  throttle3 +=step;
+			mot_Run(throttle1,throttle2,throttle3,throttle4);
+			printf("Throttle %f %f %f %f \r\n",throttle1,throttle2,throttle3,throttle4);
+		}
+
+
+		if(c=='q'){
+			mot_Stop();
+			break;
+		}
 
 		if(c=='1') {
 			printf("Run Motor1 \r\n");
-			throttle1 = .01;
+			throttle1 = step;
 			throttle2 = 0;
 			throttle3 = 0;
 			throttle4 = 0;
@@ -74,7 +146,7 @@ int main()
 		if(c=='2') {
 			printf("Run Motor2 \r\n");
 			throttle1 = 0;
-			throttle2 = .01;
+			throttle2 = step;
 			throttle3 = 0;
 			throttle4 = 0;
 			mot_Run(throttle1,throttle2,throttle3,throttle4);
@@ -83,7 +155,7 @@ int main()
 			printf("Run Motor3 \r\n");
 			throttle1 = 0;
 			throttle2 = 0;
-			throttle3 = .01;
+			throttle3 = step;
 			throttle4 = 0;
 			mot_Run(throttle1,throttle2,throttle3,throttle4);
 		}
@@ -92,15 +164,15 @@ int main()
 			throttle1 = 0;
 			throttle2 = 0;
 			throttle3 = 0;
-			throttle4 = .01;
+			throttle4 = step;
 			mot_Run(throttle1,throttle2,throttle3,throttle4);
 		}
 		if(c=='5') {
 			printf("Run All Motors\r\n");
-			throttle1 = .01;
-			throttle2 = .01;
-			throttle3 = .01;
-			throttle4 = .01;
+			throttle1 = step;
+			throttle2 = step;
+			throttle3 = step;
+			throttle4 = step;
 			mot_Run(throttle1,throttle2,throttle3,throttle4);
 		}
 		if(c==',') {
