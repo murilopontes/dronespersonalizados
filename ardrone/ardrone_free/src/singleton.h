bea141b080a0b6b9c0af978a905d31e8c58b1399
murix <murilo.pontes@gmail.com>
2014-01-10 23:30:40 -0300

    new ardrone free project

---
+++ ardrone/ardrone_free/src/singleton.h
@@ -0,0 +1,16 @@
+/*
+ * singleton.h
+ *
+ *  Created on: 02/12/2013
+ *      Author: mpontes
+ */
+
+#ifndef SINGLETON_H_
+#define SINGLETON_H_
+
+
+
+void singleton(char* program_name);
+
+
+#endif /* SINGLETON_H_ */
