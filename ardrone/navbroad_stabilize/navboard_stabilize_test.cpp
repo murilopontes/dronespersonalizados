c45bed315f46f0cbef4df735a394de9f0ad05654
murix <murilo.pontes@gmail.com>
2014-01-12 08:55:30 -0300

    new test stabilize

--- ardrone/navbroad_stabilize/navboard_stabilize_test.cpp
+++ ardrone/navbroad_stabilize/navboard_stabilize_test.cpp
@@ -23,7 +23,7 @@
 #include "../open_ardrone_v1_lib/PID_v1.h"
 
 
-void make_stable(double kp,double ki, double kd){
+void make_stable(void){
 
 	//////////////////////////////////////////////////////////////
 	int fd = open("/dev/ttyPA2", O_RDWR | O_NOCTTY | O_NDELAY);
@@ -78,7 +78,7 @@ void make_stable(double kp,double ki, double kd){
 	  previous_error = error
 	  wait(dt)
 	  goto start
-	*/
+	 */
 	//
 	double PID_H_Setpoint, PID_H_Input, PID_H_Output;
 	double PID_PITCH_Setpoint,PID_PITCH_Input,PID_PITCH_Output;
@@ -86,62 +86,70 @@ void make_stable(double kp,double ki, double kd){
 	//
 	PID_H_Input = 0;
 	PID_H_Setpoint = 30;
-    PID_PITCH_Input=0;
-    PID_PITCH_Setpoint=0;
-    PID_ROLL_Input=0;
-    PID_ROLL_Setpoint=0;
-    //
-	PID     PID_H_Control(&PID_H_Input    ,&PID_H_Output    ,&PID_H_Setpoint    ,6.4,0.025,24,DIRECT);
-    PID PID_PITCH_Control(&PID_PITCH_Input,&PID_PITCH_Output,&PID_PITCH_Setpoint,3.3,0.030,23,DIRECT);
-    PID  PID_ROLL_Control(&PID_ROLL_Input ,&PID_ROLL_Output ,&PID_ROLL_Setpoint ,3.3,0.030,23,DIRECT);
-    //
+	PID_PITCH_Input=0;
+	PID_PITCH_Setpoint=0;
+	PID_ROLL_Input=0;
+	PID_ROLL_Setpoint=0;
+
+	//
+	double ku=1.0;
+	double tu=1.0;
+	//
+	double kp=0.8*ku;
+	double ki=2*kp/tu;
+	double kd=kp*tu/8;
+	//
+	PID     PID_H_Control(&PID_H_Input    ,&PID_H_Output    ,&PID_H_Setpoint    ,kp,ki,kd,DIRECT);
+	PID PID_PITCH_Control(&PID_PITCH_Input,&PID_PITCH_Output,&PID_PITCH_Setpoint,kp,ki,kd,DIRECT);
+	PID  PID_ROLL_Control(&PID_ROLL_Input ,&PID_ROLL_Output ,&PID_ROLL_Setpoint ,kp,ki,kd,DIRECT);
+	//
 	PID_H_Control.SetSampleTime(100);
-    PID_PITCH_Control.SetSampleTime(100);
-    PID_ROLL_Control.SetSampleTime(100);
-    //
+	PID_PITCH_Control.SetSampleTime(100);
+	PID_ROLL_Control.SetSampleTime(100);
+	//
 	PID_H_Control.SetOutputLimits(0.10,0.60);
-    PID_PITCH_Control.SetOutputLimits(0.00,0.02);
-    PID_ROLL_Control.SetOutputLimits(0.00,0.02);
-    //
+	PID_PITCH_Control.SetOutputLimits(0.00,0.01);
+	PID_ROLL_Control.SetOutputLimits(0.00,0.01);
+	//
 	PID_H_Control.SetMode(AUTOMATIC);
-    PID_PITCH_Control.SetMode(AUTOMATIC);
-    PID_ROLL_Control.SetMode(AUTOMATIC);
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+	PID_PITCH_Control.SetMode(AUTOMATIC);
+	PID_ROLL_Control.SetMode(AUTOMATIC);
+	/////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    char udp_buffer[1000];
+	char udp_buffer[1000];
 	uint8_t buffer[128];
 	double height=0;
 
-    double sum_pitch_calibration=0;
-    double sum_roll_calibration=0;
-    int calibration_qtd=200;
-    ///////////////////////
-    double sum_ax=0;
-    double sum_ay=0;
-    double sum_az=0;
-    ////////////////////////
-    double sum_gx=0;
-    double sum_gy=0;
-    double sum_gz=0;
+	double sum_pitch_calibration=0;
+	double sum_roll_calibration=0;
+	int calibration_qtd=200;
+	///////////////////////
+	double sum_ax=0;
+	double sum_ay=0;
+	double sum_az=0;
+	////////////////////////
+	double sum_gx=0;
+	double sum_gy=0;
+	double sum_gz=0;
 
-    printf("navboard cabibration samples=%d wait...\n",calibration_qtd);
+	printf("navboard cabibration samples=%d wait...\n",calibration_qtd);
 	for(int l=0;l<calibration_qtd;l++){
 		////////////////////////////////////////////////////////////
 		int count_ok=0;
 		int count_rest=52;
 		do {
-		 //
-	     int n = read(fd, &buffer[count_ok], count_rest);
-	     if(n<0) continue;
-	     count_ok+=n;
-	     count_rest-=n;
+			//
+			int n = read(fd, &buffer[count_ok], count_rest);
+			if(n<0) continue;
+			count_ok+=n;
+			count_rest-=n;
 		} while(count_ok<52);
-       ///////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////
 		uint16_t *packet = (uint16_t*)&buffer;
 		/////////////////////////////////////////////////////////
 		if(packet[17] < (400*37.5f)){
-		   height=packet[17] / 37.5f;
+			height=packet[17] / 37.5f;
 		}
 		/////////////////////////////////////////////////////////
 		double ax=(packet[2]);
@@ -167,7 +175,7 @@ void make_stable(double kp,double ki, double kd){
 		double aroll  =   atan2(ay,az) * radian2degree;
 		sum_pitch_calibration+=apitch;
 		sum_roll_calibration+=aroll;
-		*/
+		 */
 	}
 
 	double ax_off=sum_ax/(double)calibration_qtd;
@@ -196,42 +204,51 @@ void make_stable(double kp,double ki, double kd){
 
 	double pitch_offset=sum_pitch_calibration/(double)calibration_qtd;
 	double roll_offset=sum_roll_calibration/(double)calibration_qtd;
-    printf("navboard calibration done pitch_offset=%.2f roll_offset=%.2f\n",pitch_offset,roll_offset);
+	printf("navboard calibration done pitch_offset=%.2f roll_offset=%.2f\n",pitch_offset,roll_offset);
 
-    /*
+	/*
     if(fabs(pitch_offset)>6 || fabs(roll_offset)>6){
     	printf("navboard calibration error exit\n");
     	exit(1);
     }
-    */
+	 */
 
-    printf("navboard started udp broadcast\n");
+	printf("navboard started udp broadcast\n");
 
 
-    double g_pitch = 0;
-    double g_roll = 0;
-    double g_yaw = 0;
+	double g_pitch = 0;
+	double g_roll = 0;
+	double g_yaw = 0;
 
-    double ts_back = util_timestamp();
-    double ts_now = util_timestamp();
+	double ts_back = util_timestamp();
+	double ts_now = util_timestamp();
 
-    double fusion_pitch = 0.0;
-    double fusion_roll = 0.0;
+	double fusion_pitch = 0.0;
+	double fusion_roll = 0.0;
 
-    uint32_t bad_count=0;
+	uint32_t bad_count=0;
+
+	double height_setpoint=30.0;
+	double height_speed=0.0;
+
+	double pitch_setpoint=0.0;
+	double pitch_speed=0.0;
+
+	double roll_setpoint=0.0;
+	double roll_speed=0.0;
 
 	for(;;){
 		////////////////////////////////////////////////////////////
 		int count_ok=0;
 		int count_rest=52;
 		do {
-		 //
-	     int n = read(fd, &buffer[count_ok], count_rest);
-	     if(n<0) continue;
-	     count_ok+=n;
-	     count_rest-=n;
+			//
+			int n = read(fd, &buffer[count_ok], count_rest);
+			if(n<0) continue;
+			count_ok+=n;
+			count_rest-=n;
 		} while(count_ok<52);
-       ///////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////
 		uint16_t *packet = (uint16_t*)&buffer;
 
 		/*
@@ -239,7 +256,7 @@ void make_stable(double kp,double ki, double kd){
 		printf("%04x|",packet[i]);
 		}
 		printf("\n");
-		*/
+		 */
 		if(packet[0]!=0x0032){
 			printf("drop bad read=%d\n",bad_count);
 			bad_count++;
@@ -248,13 +265,13 @@ void make_stable(double kp,double ki, double kd){
 
 		/////////////////////////////////////////////////////////
 		if(packet[17] /37.5f < 650 ){
-		   height=packet[17] / 37.5f;
+			height=packet[17] / 37.5f;
 		}
 		/////////////////////////////////////////////////////////
 		double ax=(packet[2] - ax_off)/100.0f;
 		double ay=(packet[3] - ay_off)/100.0f;
 		double az=(packet[4] - az_off)/100.0f;
-        double radian2degree = 57.295779513082320876798154814105f;
+		double radian2degree = 57.295779513082320876798154814105f;
 		double apitch = ( -atan2(ax,az) * radian2degree );
 		double aroll  = (  atan2(ay,az) * radian2degree );
 		/////////////////////////////////////////////////////////
@@ -293,50 +310,79 @@ void make_stable(double kp,double ki, double kd){
 		udpClient_Send(&udp_vertical, udp_buffer, strlen(udp_buffer));
 
 		///////////////////////////////////////////////////////////
-/*
 
- 	    PID_H_Input = height;
+
+		PID_H_Input = height;
 		PID_H_Control.Compute();
 
-		PID_PITCH_Input=apitch;
+		PID_PITCH_Input=fusion_pitch;
 		PID_PITCH_Control.Compute();
 
-		PID_ROLL_Input=aroll;
+		PID_ROLL_Input=fusion_roll;
 		PID_ROLL_Control.Compute();
 
 		/////////////////////////////////////////////////////////////
 
+		/*
 		double adj_yaw=0;
-
 		motor[0] = PID_H_Output  +PID_ROLL_Output -PID_PITCH_Output -adj_yaw;
 		motor[1] = PID_H_Output  -PID_ROLL_Output -PID_PITCH_Output +adj_yaw;
 		motor[2] = PID_H_Output  -PID_ROLL_Output +PID_PITCH_Output -adj_yaw;
 		motor[3] = PID_H_Output  +PID_ROLL_Output +PID_PITCH_Output +adj_yaw;
-*/
-		//mot_Run(motor[0],motor[1],motor[2],motor[3]);
+		 */
+
+		double height_error = height_setpoint - height ;
+		if(height_error>0){
+			//printf("muito baixo -> accelerar -> ");
+			height_speed+=0.01/10.0f;
+		}
+		if(height_error<0){
+			//printf("muito alto -> desaccelerar -> ");
+			height_speed-=0.01/10.0f;
+		}
+		if(height_speed<0.20) height_speed=0.20;
+		if(height_speed>0.60) height_speed=0.60;
+
+		printf("height_error=(%.2f-%.2f)=%.2f height_speed=%.2f\n",height_setpoint,height,height_error,height_speed);
+
+		double pitch_error = pitch_setpoint - fusion_pitch;
+		if(pitch_error>0){
+			pitch_speed+=0.01/10.0f;
+		}
+		if(pitch_error<0){
+			pitch_speed-=0.01/10.0f;
+		}
+		if(pitch_speed<0) pitch_speed=0;
+		if(pitch_speed>5) pitch_speed=5;
+
+		double roll_error = roll_setpoint - fusion_roll;
+		if(roll_error>0){
+			roll_speed+=0.01/10.0f;
+		}
+		if(roll_error<0){
+			roll_speed-=0.01/10.0f;
+		}
+		if(roll_speed<0) roll_speed=0;
+		if(roll_speed>5) roll_speed=5;
+
+
+		motor[0] = height_speed -pitch_speed + roll_speed;
+		motor[1] = height_speed -pitch_speed - roll_speed;
+		motor[2] = height_speed +pitch_speed - roll_speed;
+		motor[3] = height_speed +pitch_speed + roll_speed;
+
+		mot_Run(motor[0],motor[1],motor[2],motor[3]);
+
 		///////////////////////////////////////////////////////////////
-        /*
+		/*
 		printf("h=%.2f(adj=%.2f) pitch=%.2f(adj=%.2f) roll=%.2f(adj=%.2f)  m=|%.2f|%.2f|%.2f|%.2f|\r\n",
 				height,PID_H_Output,
 				apitch,PID_PITCH_Output,
 				aroll,PID_ROLL_Output,
 				motor[0],motor[1],motor[2],motor[3]);
-*/
-
-
-
-		//////////////////////////////////////////////////////////
-		//printf("pitch=%.2f roll=%.2f h=%.2f\r\n",apitch,aroll,height);
-		
-
-
-	    //
-		//usleep(5000);
+		 */
 	}
 
-	//init nav structure
-    //nav->ts = util_timestamp();
-    //nav->dt = 0;
 }
 
 
@@ -362,7 +408,7 @@ int main(int argc, char** argv)
 	system("sysctl -w kernel.panic=0");
 	system("sysctl -w kernel.panic_on_oops=0");
 
-	make_stable(0,0,0);
+	make_stable();
 
 
 
