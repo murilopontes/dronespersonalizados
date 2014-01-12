0d623bfefa8d361764fcbf90b7093f6c8fa6a269
murix <murilo.pontes@gmail.com>
2014-01-12 17:17:29 -0300

    multiwii 2.3 original code

---
+++ ardrone/multiwii_2_3_ardrone/Arduino.cpp
@@ -0,0 +1,21 @@
+/*
+ * Arduino.cpp
+ *
+ *  Created on: 12/01/2014
+ *      Author: mpontes
+ */
+
+
+
+#include "Arduino.h"
+
+int millis(void){}
+int micros(void){}
+void delay(int){}
+int constrain(int x, int a, int b){}
+int min(int a,int b){}
+int max(int a,int b){}
+int sq(int a){}
+
+uint8_t PWM_PIN[8];
+
