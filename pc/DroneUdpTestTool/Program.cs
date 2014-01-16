eef6d9c90926559c831f7499bbdbe8bd43deb5f2
murix <murilo.pontes@gmail.com>
2014-01-16 17:44:41 -0300

    udp test tool

---
+++ pc/DroneUdpTestTool/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DroneUdpTestTool
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // This constructor arbitrarily assigns the local port number.
+            UdpClient udpClient = new UdpClient();
+            try
+            {
+                udpClient.Connect("192.168.1.1", 3000);
+
+                int tx_count = 0;
+                int rx_count = 0;
+
+                while (true)
+                {
+                    // Sends a message to the host to which you have connected.
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+                    udpClient.Send(sendBytes, sendBytes.Length);
+                    tx_count++;
+                    Console.WriteLine("TX[" + tx_count + "]");
+                    if (udpClient.Available > 0) {
+                        //IPEndPoint object will allow us to read datagrams sent from any source.
+                        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                        // Blocks until a message returns on this socket from a remote host.
+                        Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                        rx_count++;
+                        string returnData = Encoding.ASCII.GetString(receiveBytes);
+                        // Uses the IPEndPoint object to determine which of these two hosts responded.
+                        Console.WriteLine("RX["+rx_count+"]=" +  returnData.ToString());
+                        Console.WriteLine("RX info=" + RemoteIpEndPoint.Address.ToString() + ":" + RemoteIpEndPoint.Port.ToString());
+                        
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
