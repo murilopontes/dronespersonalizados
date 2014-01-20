7f4bef89c70f59431627048cc5953fb8ee5c0554
murix <murilo.pontes@gmail.com>
2014-01-20 20:17:19 -0300

    new raw navboard json

--- ardrone/drone/murix_navboard.cpp
+++ ardrone/drone/murix_navboard.cpp
@@ -13,15 +13,25 @@
 #include "murix_gpio.h"
 #include "murix_constraint.h"
 
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+boost::atomic<navboard_fusion_t> atomic_navboard_fusion;
+boost::atomic<navboard_raw_calibration_t> atomic_navboard_raw_calibration;
+boost::atomic<bool> navboard_fusion_ready(false);
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 
 double radian2degree(double radian){
 	return radian * (180.0f / M_PI);
 }
 
-
-
-
-
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 
 navboard_raw_calibration_t::navboard_raw_calibration_t(){
 
@@ -36,17 +46,124 @@ navboard_raw_calibration_t::navboard_raw_calibration_t(){
 	gyro_gain = (180.0f / M_PI) / 10.0f;
 }
 
+//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+navboard_fusion_t::navboard_fusion_t(){
+
+	//
+	memset(&navboard_raw,0,sizeof(navboard_raw));
+
+	//
+	pkt_raw = (navboard_raw_t*)& navboard_raw;
+
+	//
+	acc_x=0;
+	acc_y=0;
+	acc_z=0;
+
+	//
+	gyro_x=0;
+	gyro_y=0;
+	gyro_z=0;
+
+	//
+	gyro_x_dt=0;
+	gyro_y_dt=0;
+	gyro_z_dt=0;
+
+	//
+	gyro_x_integrate=0;
+	gyro_y_integrate=0;
+	gyro_z_integrate=0;
+
+	//
+	acc_pitch=0;
+	acc_roll=0;
+
+	//
+	fusion_pitch=0;
+	fusion_roll=0;
+
+	//
+	height=0;
+
+	//
+	dt=0;
+	seq=0;
+}
+
+void navboard_fusion_t::Compute(navboard_raw_calibration_t calib,double dt){
 
 
-boost::atomic<navboard_raw_calibration_t> atomic_navboard_raw_calibration;
-boost::atomic<navboard_raw_t> atomic_navboard_raw;
-boost::atomic<navboard_fusion_t> atomic_navboard_fusion;
-boost::atomic<bool> navboard_raw_ready(false);
-boost::atomic<bool> navboard_fusion_ready(false);
+	this->dt = dt;
+	this->seq = this->pkt_raw->seq;
+
+	//offset and scale
+	this->acc_x= (this->pkt_raw->acc_x - calib.acc_x_offset);
+	this->acc_y= (this->pkt_raw->acc_y - calib.acc_y_offset);
+	this->acc_z= (this->pkt_raw->acc_z - calib.acc_z_offset);
+
+	//
+	this->gyro_x = (this->pkt_raw->gyro_x - calib.gyro_x_offset)/calib.gyro_gain;
+	this->gyro_y = (this->pkt_raw->gyro_y - calib.gyro_y_offset)/calib.gyro_gain;
+	this->gyro_z = (this->pkt_raw->gyro_z - calib.gyro_z_offset)/calib.gyro_gain;
+
+	///////////////////////////////////////////////////////////////////////////////////
+
+	//double pitch_tmp = sqrt((this->acc_y*this->acc_y)+(this->acc_z*this->acc_z));
+
+	//
+	this->acc_pitch =  radian2degree(  atan2(this->acc_x,this->acc_z)  );
+
+	//
+	this->acc_roll =   radian2degree(  atan2(this->acc_y,this->acc_z)  );
 
 
+	///////////////////////////////////////////////////////////////////////////////
+	//prepare to integrate
+	this->gyro_x_dt= this->gyro_x*dt;
+	this->gyro_y_dt= this->gyro_y*dt;
+	this->gyro_z_dt= this->gyro_z*dt;
 
-void navboard_read_raw(void)
+	//integrate
+	this->gyro_x_integrate+= this->gyro_x_dt;
+	this->gyro_y_integrate+= this->gyro_y_dt;
+	this->gyro_z_integrate+= this->gyro_z_dt;
+
+	//clamp integrate
+	this->gyro_x_integrate=constraint_double(this->gyro_x_integrate,-90,90);
+	this->gyro_y_integrate=constraint_double(this->gyro_y_integrate,-180,180);
+	this->gyro_z_integrate=constraint_double(this->gyro_z_integrate,-180,180);
+
+	//////////////////////////////////////////////////////////////////////////////
+	//use The complementary filter for pitch and roll
+
+	this->fusion_pitch= 0.98*(this->fusion_pitch+this->gyro_x_dt)+(0.02*this->acc_pitch);
+	this->fusion_roll = 0.98*(this->fusion_roll+this->gyro_y_dt)+(0.02*this->acc_roll);
+
+	this->fusion_pitch=constraint_double(this->fusion_pitch,-90,90);
+	this->fusion_roll=constraint_double(this->fusion_roll,-180,180);
+
+	/////////////////////////////////////////////////////////////////////////////
+
+	this->height= this->pkt_raw->us_echo & 0x7FFF;
+
+	/*
+	if(this->pkt_raw->us_echo / 37.5f < 650){
+		this->height=this->pkt_raw->us_echo / 37.5f;
+	}
+	*/
+
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+
+void navboard_generator(void)
 {
 
 
@@ -59,16 +176,10 @@ void navboard_read_raw(void)
 	port.open("/dev/ttyPA2");
 	port.set_option(boost::asio::serial_port_base::baud_rate(460800));
 
-
 	//
 	uint8_t navboard_size=0x32;
 	uint8_t c;
 	boost::circular_buffer<uint8_t> cb(navboard_size);
-	//
-	navboard_raw_t packet;
-	uint8_t navboard8[navboard_size];
-	uint16_t* p_navboard16;
-	p_navboard16 = (uint16_t*)& navboard8;
 
 	//set /MCLR pin -> reset navboard
 	gpio_set(132,GPIO_OUT_1);
@@ -78,11 +189,22 @@ void navboard_read_raw(void)
 	boost::asio::write(port, boost::asio::buffer(&cmd, 1));
 
 	//
-	if(sizeof(navboard_raw_t)!=sizeof(navboard8)){
-		printf("%d!=%d\r\n",sizeof(navboard_raw_t),sizeof(navboard8));
+	if(sizeof(navboard_raw_t)!=navboard_size){
+		printf("%d!=%d\r\n",sizeof(navboard_raw_t),navboard_size);
 		printf("error %s:%d\r\n",__FILE__,__LINE__);
 		exit(1);
 	}
+
+
+	//
+	boost::posix_time::ptime tick_back;
+	boost::posix_time::ptime tick_now;
+	boost::posix_time::time_duration tick_diff;
+	//
+	tick_back = boost::posix_time::microsec_clock::local_time();
+	tick_now = boost::posix_time::microsec_clock::local_time();
+
+	navboard_fusion_ready=true;
 
 	//
 	printf("navboard reader working\r\n");
@@ -96,13 +218,24 @@ void navboard_read_raw(void)
 			//header of buffer is navboard signature
 			if(cb[0]==navboard_size && cb[1]==0x0 && cb[31]==0x0 && cb[30]==0x1
 			){
-				//copy packet to linear buffer
+				////////////////////////////////////////////////////////////////
+				tick_back = tick_now;
+				tick_now = boost::posix_time::microsec_clock::local_time();
+				tick_diff = tick_now - tick_back;
+				double dt = tick_diff.total_nanoseconds() / 1000000000.0f;
+				///////////////////////////////////////////////////////////////
+				navboard_fusion_t fusion = navboard_fusion_t();
 				for(int i=0;i<navboard_size;i++){
-					navboard8[i]=cb[i];
+					fusion.navboard_raw[i]=cb[i];
 				}
-				memcpy(&packet,&navboard8,sizeof(packet));
-				atomic_navboard_raw = packet;
-				navboard_raw_ready = true;
+				////////////////////////////////////////////////////////////////
+				navboard_raw_calibration_t calib;// = atomic_navboard_raw_calibration;
+				fusion.Compute(calib,dt);
+				////////////////////////////////////////////////////////////////
+				//atomic_navboard_fusion=fusion;
+				boost::this_thread::sleep(boost::posix_time::milliseconds(1));
+
+
 			}
 		}
 	}
@@ -112,179 +245,70 @@ void navboard_read_raw(void)
 
 
 
-
-
-void navboard_raw_to_fusion(navboard_raw_t* packet,navboard_fusion_t* fusion,double dt){
-
-	//
-	navboard_raw_calibration_t navboard_calibration;
-	navboard_calibration = atomic_navboard_raw_calibration;
-
-	fusion->dt = dt;
-	fusion->seq = packet->seq;
-
-	//offset and scale
-	fusion->acc_x= (packet->acc_x - navboard_calibration.acc_x_offset);
-	fusion->acc_y= (packet->acc_y - navboard_calibration.acc_y_offset);
-	fusion->acc_z= (packet->acc_z - navboard_calibration.acc_z_offset);
-
-	//
-	fusion->gyro_x = (packet->gyro_x - navboard_calibration.gyro_x_offset)/navboard_calibration.gyro_gain;
-	fusion->gyro_y = (packet->gyro_y - navboard_calibration.gyro_y_offset)/navboard_calibration.gyro_gain;
-	fusion->gyro_z = (packet->gyro_z - navboard_calibration.gyro_z_offset)/navboard_calibration.gyro_gain;
-
-	///////////////////////////////////////////////////////////////////////////////////
-
-	double pitch_tmp = sqrt((fusion->acc_y*fusion->acc_y)+(fusion->acc_z*fusion->acc_z));
-
-	//
-	fusion->acc_pitch =  radian2degree(  atan2(fusion->acc_x,pitch_tmp)  );
-
-	//
-	fusion->acc_roll =   radian2degree(  atan2(fusion->acc_y,fusion->acc_z)  );
-
-
-	///////////////////////////////////////////////////////////////////////////////
-	//prepare to integrate
-	fusion->gyro_x_dt= fusion->gyro_x*dt;
-	fusion->gyro_y_dt= fusion->gyro_y*dt;
-	fusion->gyro_z_dt= fusion->gyro_z*dt;
-
-	//integrate
-	fusion->gyro_x_integrate+= fusion->gyro_x_dt;
-	fusion->gyro_y_integrate+= fusion->gyro_y_dt;
-	fusion->gyro_z_integrate+= fusion->gyro_z_dt;
-
-	//clamp integrate
-	fusion->gyro_x_integrate=constraint_double(fusion->gyro_x_integrate,-90,90);
-	fusion->gyro_y_integrate=constraint_double(fusion->gyro_y_integrate,-180,180);
-	fusion->gyro_z_integrate=constraint_double(fusion->gyro_z_integrate,-180,180);
-
-	//////////////////////////////////////////////////////////////////////////////
-	//use The complementary filter for pitch and roll
-
-	fusion->fusion_pitch= 0.98*(fusion->fusion_pitch+fusion->gyro_x_dt)+(0.02*fusion->acc_pitch);
-	fusion->fusion_roll = 0.98*(fusion->fusion_roll+fusion->gyro_y_dt)+(0.02*fusion->acc_roll);
-
-	fusion->fusion_pitch=constraint_double(fusion->fusion_pitch,-90,90);
-	fusion->fusion_roll=constraint_double(fusion->fusion_roll,-180,180);
-
-	/////////////////////////////////////////////////////////////////////////////
-
-	if(packet->us_echo / 37.5f < 650){
-		fusion->height=packet->us_echo / 37.5f;
-	}
-
-}
-
-
-
-void navboard_consumer_raw_procude_fusion(void){
-
-	//
-	navboard_raw_t navboard_raw;
-	navboard_fusion_t navboard_fusion;
-
-	//
-	boost::posix_time::ptime tick_back;
-	boost::posix_time::ptime tick_now;
-	boost::posix_time::time_duration tick_diff;
-
-	//
-	tick_back = boost::posix_time::microsec_clock::local_time();
-	tick_now = boost::posix_time::microsec_clock::local_time();
-
-	//
-	navboard_fusion.height=24;
-
-	//
-	while(!navboard_raw_ready){
-		printf("wait navboard_raw_ready\r\n");
-		boost::this_thread::sleep(boost::posix_time::milliseconds(16));
-	}
-
-	printf("navboard working!\r\n");
-	while(true){
-		///////////////////////////////////////////////////////////////
-		navboard_raw = atomic_navboard_raw;
-		///////////////////////////////////////////////////////////////
-		tick_back = tick_now;
-		tick_now = boost::posix_time::microsec_clock::local_time();
-		tick_diff = tick_now - tick_back;
-		double dt = tick_diff.total_nanoseconds() / 1000000000.0f;
-		//////////////////////////////////////////////////////////////
-		navboard_raw_to_fusion(&navboard_raw,&navboard_fusion,dt);
-		atomic_navboard_fusion = navboard_fusion;
-		navboard_fusion_ready = true;
-		///////////////////////////////////////////////////////////////
-	}
-}
-
-
-void navboard_and_motors_and_vbat_udp_server(void){
-
-	navboard_fusion_t fusion;
-	navboard_raw_calibration_t calib;
-
+void navboard_udp_json_server(void){
 
 	uint32_t count=0;
 	enum { max_length = 1024 };
 	char data[max_length];
 
 	boost::asio::io_service io_service;
-	boost::asio::ip::udp::socket sock(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 4000));
-
+	boost::asio::ip::udp::socket sock(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 5000));
 
 	//
 	while(!navboard_fusion_ready ){
-		printf("wait navboard_fusion_ready & vbat_ready");
-		boost::this_thread::sleep(boost::posix_time::milliseconds(16));
+		printf("navboard_udp_json_server wait navboard_fusion_ready\r\n");
+		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
 
-	printf("navboard_udp_show ok!\r\n");
+	printf("navboard_udp_json_server ok!\r\n");
 	while(true)
 	{
-
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
 		boost::asio::ip::udp::endpoint sender_endpoint;
 		size_t length = sock.receive_from(boost::asio::buffer(data, max_length), sender_endpoint);
 		data[length]=0;
 		count++;
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
 
+		navboard_fusion_t fusion;
+		navboard_raw_calibration_t calib;
 		fusion = atomic_navboard_fusion;
 		calib = atomic_navboard_raw_calibration;
 
 
+		boost::property_tree::ptree pt;
 
-		sock.send_to(boost::asio::buffer(data, length), sender_endpoint);
+		//raw data for help calibration
+		pt.put("raw_ac_x",fusion.pkt_raw->acc_x);
+		pt.put("raw_ac_x",fusion.pkt_raw->acc_y);
+		pt.put("raw_ac_z",fusion.pkt_raw->acc_z);
+		pt.put("raw_gy_x",fusion.pkt_raw->gyro_x);
+		pt.put("raw_gy_y",fusion.pkt_raw->gyro_y);
+		pt.put("raw_gy_z",fusion.pkt_raw->gyro_z);
+		pt.put("raw_echo",fusion.pkt_raw->us_echo);
+
+
+		std::stringstream ss;
+	    boost::property_tree::json_parser::write_json(ss, pt);
+
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+
+		sock.send_to(boost::asio::buffer(ss.str().c_str(), ss.str().length()), sender_endpoint);
+
+
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////
+
 	}
 
 
 }
 
-void navboard_consumer_show_in_console(void){
-	navboard_fusion_t fusion;
-	//
-	while(!navboard_fusion_ready){
-		printf("wait navboard_fusion\r\n");
-		boost::this_thread::sleep(boost::posix_time::milliseconds(16));
-	}
-	while(true){
-		fusion = atomic_navboard_fusion;
-		printf("dt=%.6f seq=%.0f a(%.2f|%.2f) g(%.2f|%.2f|%.2f) gi(%.2f|%.2f|%.2f) f(%.2f|%.2f) h=%.2f\r\n",
-				fusion.dt,
-				fusion.seq,
-				fusion.acc_pitch,
-				fusion.acc_roll,
-				fusion.gyro_x,
-				fusion.gyro_y,
-				fusion.gyro_z,
-				fusion.gyro_x_integrate,
-				fusion.gyro_y_integrate,
-				fusion.gyro_z_integrate,
-				fusion.fusion_pitch,
-				fusion.fusion_roll,
-				fusion.height
-		);
-	}
-}
+
 
--- ardrone/drone/murix_navboard.h
+++ ardrone/drone/murix_navboard.h
@@ -14,43 +14,74 @@
 typedef struct
 {
 	//
-	uint16_t size;                // +0x00 Size of the following data (always 0x2C)
-	uint16_t seq;                 // +0x02 Sequence number, increases every update
+	uint16_t size;
+	//
+	uint16_t seq;
 
 	//Bosh BMA150
-	uint16_t acc_x;              // +0x04 Raw data (10-bit) of the accelerometers multiplied by 4
-	uint16_t acc_y;              // +0x04 Raw data (10-bit) of the accelerometers multiplied by 4
-	uint16_t acc_z;              // +0x04 Raw data (10-bit) of the accelerometers multiplied by 4
+	uint16_t acc_x;
+	uint16_t acc_y;
+	uint16_t acc_z;
 
 	//Invensense IDG-500 max=+-500 - 2mV g/s  / +-110 - 9.1mV g/s
-	uint16_t gyro_x;             // +0x0A Raw data for the gyros, 12-bit A/D converted voltage of the gyros. X,Y=IDG, Z=Epson
-	uint16_t gyro_y;             // +0x0A Raw data for the gyros, 12-bit A/D converted voltage of the gyros. X,Y=IDG, Z=Epson
+	uint16_t gyro_x;
+	uint16_t gyro_y;
 
 	//Epson XV-3500CB max=100 degrees/s ---- 0.67 mV/(degree/s)
 	uint16_t gyro_z;
 
-	uint16_t gyro_110_x;         // +0x10 4.5x Raw data (IDG), gyro values with another resolution (see IDG-500 datasheet)
-	uint16_t gyro_110_y;         // +0x10 4.5x Raw data (IDG), gyro values with another resolution (see IDG-500 datasheet)
-	uint16_t acc_temp;            // +0x14 Accs temperature -- startup value 120 @ 25C, rising to 143
-	uint16_t gyro_temp;           // +0x16 XYGyro temperature (IDG), 12-bit A/D converted voltage of IDG's temperature sensor -- startup value 1532 @ 25C, rising to 1572
-	uint16_t vrefEpson;           // +0x18 ZGyro v_ref (Epson), 12-bit A/D converted reference voltage of the Epson sensor
-	uint16_t vrefIDG;             // +0x1A XYGyro v_ref (IDG), 12-bit A/D converted reference voltage of the IDG sensor
-	uint16_t us_echo;             // +0x1C bit15=1 echo pulse transmitted, bit14-0 first echo. Value 30 = 1cm. min value: 784 = 26cm
-	uint16_t checksum;            // +0x1E Checksum = sum of all values except checksum (22 values)
-	uint16_t us_echo_start;       // +0x20 Array with starts of echos (8 array values @ 25Hz, 9 values @ 22.22Hz)
-	uint16_t us_echo_end;         // +0x22 Array with ends of echos   (8 array values @ 25Hz, 9 values @ 22.22Hz)
-	uint16_t us_association_echo; // +0x24 Ultrasonic parameter -- echo number starting with 0. max value 3758. examples: 0,1,2,3,4,5,6,7  ; 0,1,2,3,4,86,6,9
-	uint16_t us_distance_echo;    // +0x26 Ultrasonic parameter -- no clear pattern
-	uint16_t us_courbe_temps;     // +0x28 Ultrasonic parameter -- counts up from 0 to approx 24346 in 192 sample cycles of which 12 cylces have value 0
-	uint16_t us_courbe_valeur;    // +0x2A Ultrasonic parameter -- value between 0 and 4000, no clear pattern. 192 sample cycles of which 12 cylces have value 0
-	uint16_t us_courbe_ref;       // +0x2C Ultrasonic parameter -- coutns down from 4000 to 0 in 192 sample cycles of which 12 cylces have value 0
-	uint16_t uk01;
-	uint16_t uk02;
+	//Invensense IDG-500
+	uint16_t gyro_110_x;
+	uint16_t gyro_110_y;
+
+	//BMA150 (120=25C, 143=48C, offset=-95)
+	uint16_t acc_temp;
+
+	//Invensense IDG-500 (1532=25C, 1572=65, offset=-1507)
+	uint16_t gyro_temp;
+
+	//Epson XV-3500CB
+	uint16_t vrefEpson;
+
+	//Invensense IDG-500
+	uint16_t vrefIDG;
+
+	//echo (bit15=signal transmitted, bit14-0=echo value)
+	uint16_t us_echo;
+
+	//
+	uint16_t checksum;
+
+	uint16_t us_echo_start;
+	uint16_t us_echo_end;
+	uint16_t us_association_echo;
+	uint16_t us_distance_echo;
+	uint16_t us_courbe_temps;
+	uint16_t us_courbe_valeur;
+	uint16_t us_courbe_ref;
+	uint16_t us_unknown01;
+	uint16_t us_unknown02;
 } navboard_raw_t;
+
+
+class navboard_raw_calibration_t {
+public:
+	double acc_x_offset;
+	double acc_y_offset;
+	double acc_z_offset;
+	double gyro_x_offset;
+	double gyro_y_offset;
+	double gyro_z_offset;
+	double gyro_gain;
+
+	navboard_raw_calibration_t();
+} ;
 
 
 class navboard_fusion_t {
 public:
+	uint8_t navboard_raw[50];
+	navboard_raw_t* pkt_raw;
 
 	double acc_x;
 	double acc_y;
@@ -80,27 +111,18 @@ public:
 	double dt;
 	double seq;
 
+	navboard_fusion_t();
+	void Compute(navboard_raw_calibration_t calibration_data,double dt);
 };
 
-class navboard_raw_calibration_t {
-public:
-	double acc_x_offset;
-	double acc_y_offset;
-	double acc_z_offset;
-	double gyro_x_offset;
-	double gyro_y_offset;
-	double gyro_z_offset;
-	double gyro_gain;
 
-	navboard_raw_calibration_t();
-} ;
-
-
-extern boost::atomic<navboard_raw_calibration_t> atomic_navboard_raw_calibration;
-extern boost::atomic<navboard_raw_t> atomic_navboard_raw;
 extern boost::atomic<navboard_fusion_t> atomic_navboard_fusion;
-extern boost::atomic<bool> navboard_raw_ready;
+extern boost::atomic<navboard_raw_calibration_t> atomic_navboard_calibration;
 extern boost::atomic<bool> navboard_fusion_ready;
 
+
+
+void navboard_generator(void);
+void navboard_udp_json_server(void);
 
 #endif /* MURIX_NAVBOARD_H_ */
