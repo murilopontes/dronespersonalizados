2d73d262d9a2df6e1a1600482bc8cc0fc144a83c
murix <murilo.pontes@gmail.com>
2014-01-28 15:15:32 -0300

    update axiom

--- pc/AxiomDX9Game/Game.cs
+++ pc/AxiomDX9Game/Game.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Axiom.Core;
 using Axiom.Graphics;
 using Axiom.Math;
+using Axiom.Input;
 
 namespace AxiomDX9Game2
 {
@@ -16,6 +17,8 @@ namespace AxiomDX9Game2
         private SceneManager _scene;
         private Camera _camera;
 
+
+
         public void OnLoad()
         {
             ResourceGroupManager.Instance.AddResourceLocation("media", "Folder", true);
@@ -23,10 +26,14 @@ namespace AxiomDX9Game2
             _scene = _engine.CreateSceneManager("DefaultSceneManager", "DefaultSM");
             _scene.ClearScene();
 
+            //_scene = Root.Instance.CreateSceneManager(SceneType.ExteriorClose);
+            //_scene.LoadWorldGeometry("Terrain.xml");
+
+
             _camera = _scene.CreateCamera("MainCamera");
 
             //
-            _camera.Position = new Vector3(0, 300, 300); // x=right/left from screen,y=bottom/up from screen,z=in/out from scren
+            _camera.Position = new Vector3(0, 170, 300); // x=right/left from screen,y=bottom/up from screen,z=in/out from scren
             _camera.LookAt(Vector3.Zero);
 
             //The clipping distance of a Camera specifies how close or far something can be before you no longer see it. 
@@ -46,24 +53,18 @@ namespace AxiomDX9Game2
         {
 
             //shadows and ambient light
-            _scene.AmbientLight = ColorEx.White;
-           // _scene.ShadowTechnique = ShadowTechnique.StencilAdditive;
+            _scene.AmbientLight = ColorEx.Black;
+
+            //_scene.ShadowTechnique = ShadowTechnique.StencilAdditive;
+            _scene.ShadowTechnique = ShadowTechnique.StencilModulative;
+            //_scene.ShadowTechnique = ShadowTechnique.TextureAdditive;
+            //_scene.ShadowTechnique = ShadowTechnique.TextureModulative;
 
 
             Entity ent = _scene.CreateEntity("Penguin", "penguin.mesh");
+            ent.CastShadows = true;
             SceneNode node = _scene.RootSceneNode.CreateChildSceneNode("PenguinNode");
             node.AttachObject(ent);
-            
-            Entity ent2 = _scene.CreateEntity("Penguin2", "penguin.mesh");
-            //ent2.IsVisible = false;
-            //ent2.Name;
-            //ent2.ParentSceneNode;
-            ent2.CastShadows = true;
-            SceneNode node2 = _scene.RootSceneNode.CreateChildSceneNode("Penguin2", new Vector3(100, 0, 0));
-            node2.AttachObject(ent2);
-
-            //
-            node2.Position += new Vector3(10, 0, 10);
             node.Position += new Vector3(25, 0, 0);
             node.Scale = new Vector3(0.5f, 0.5f, 0.5f);
             //node2.ScaleBy(new Vector3(10, 10, 10));
@@ -72,6 +73,31 @@ namespace AxiomDX9Game2
             node.Roll(30);
             node.Position += new Vector3(0, 50, 0);
 
+            Entity ent2 = _scene.CreateEntity("Penguin2", "penguin.mesh");
+            //ent2.IsVisible = false;
+            //ent2.Name;
+            //ent2.ParentSceneNode;
+            ent2.CastShadows = true;
+            SceneNode node2 = _scene.RootSceneNode.CreateChildSceneNode("Penguin2", new Vector3(0, 0, 0));
+            node2.AttachObject(ent2);
+            node2.Position += new Vector3(-50, 25, -50);
+
+
+            //create ground
+            Plane plane = new Plane(Vector3.UnitY, 0);
+            MeshManager.Instance.CreatePlane("ground", ResourceGroupManager.DefaultResourceGroupName, plane, 300, 300, 20, 20, true, 1, 5, 5, Vector3.UnitZ);
+            Entity groundEnt = _scene.CreateEntity("GroundEntity", "ground");
+            _scene.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
+            groundEnt.CastShadows = false;
+            groundEnt.MaterialName = "Rockwall";
+
+            //create light 
+            Light pointLight = _scene.CreateLight("pointLight");
+            pointLight.Type = LightType.Point;
+            pointLight.Position = new Vector3(0, 200, 0);
+            pointLight.Diffuse = ColorEx.White;
+            pointLight.Specular = ColorEx.White;
+
         }
 
 
@@ -79,24 +105,40 @@ namespace AxiomDX9Game2
         {
         }
 
+
+        void HandleKeyboardInput(FrameEventArgs e) {
+            InputReader input = new InputReader();
+            input.Capture();
+        }
+
+
         public void OnRenderFrame(object s, FrameEventArgs e)
         {
+            HandleKeyboardInput(e);
+
         }
 
         public void Run()
         {
-            using (_engine = new Root())
-            {
-                _engine.RenderSystem = _engine.RenderSystems[0];
-                using (_window = _engine.Initialize(true))
-                {
-                    OnLoad();
-                    CreateScene();
-                    _engine.FrameRenderingQueued += OnRenderFrame;
-                    _engine.StartRendering();
-                    OnUnload();
-                }
-            }
+            //Root root = new Root("Game.log");
+            //root.RenderSystem = root.RenderSystems["DirectX9"];
+            //root.RenderSystem.ConfigOptions["Video Mode"].Value = "1280 x 720 @ 32-bit color";
+
+
+            _engine = new Root("Game.log");
+
+            _engine.RenderSystem = _engine.RenderSystems[0];
+            //_engine.RenderSystem = _engine.RenderSystems["DirectX9"];
+
+            _window = _engine.Initialize(true);
+            
+                OnLoad();
+                CreateScene();
+                _engine.FrameRenderingQueued += OnRenderFrame;
+                _engine.StartRendering();
+                OnUnload();
+            
+
         }
     }
 }
