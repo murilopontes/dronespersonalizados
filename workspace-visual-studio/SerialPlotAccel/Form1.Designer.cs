0d7e074feffe0a3076539c1387109bd55d9a6104
murix <murilo.pontes@gmail.com>
2014-01-31 01:03:15 -0300

    accelerometer position and speed

--- workspace-visual-studio/SerialPlotAccel/Form1.Designer.cs
+++ workspace-visual-studio/SerialPlotAccel/Form1.Designer.cs
@@ -29,13 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.chart_acc = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_speed = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_position = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_acc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_speed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_position)).BeginInit();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -48,32 +55,56 @@
             this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // chart1
+            // chart_acc
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_acc.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_acc.Legends.Add(legend1);
+            this.chart_acc.Location = new System.Drawing.Point(33, 30);
+            this.chart_acc.Name = "chart_acc";
+            this.chart_acc.Size = new System.Drawing.Size(629, 136);
+            this.chart_acc.TabIndex = 0;
+            this.chart_acc.Text = "chart_acc";
+            // 
+            // chart_speed
             // 
             chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart_speed.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(33, 30);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(629, 300);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            this.chart_speed.Legends.Add(legend2);
+            this.chart_speed.Location = new System.Drawing.Point(33, 172);
+            this.chart_speed.Name = "chart_speed";
+            this.chart_speed.Size = new System.Drawing.Size(629, 136);
+            this.chart_speed.TabIndex = 1;
+            this.chart_speed.Text = "chart_acc";
+            // 
+            // chart_position
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart_position.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart_position.Legends.Add(legend3);
+            this.chart_position.Location = new System.Drawing.Point(33, 314);
+            this.chart_position.Name = "chart_position";
+            this.chart_position.Size = new System.Drawing.Size(629, 136);
+            this.chart_position.TabIndex = 2;
+            this.chart_position.Text = "chart_acc";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 424);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(697, 496);
+            this.Controls.Add(this.chart_position);
+            this.Controls.Add(this.chart_speed);
+            this.Controls.Add(this.chart_acc);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_acc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_speed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_position)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,7 +113,9 @@
 
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_acc;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_speed;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_position;
     }
 }
 
--- workspace-visual-studio/SerialPlotAccel/Form1.cs
+++ workspace-visual-studio/SerialPlotAccel/Form1.cs
@@ -23,21 +23,45 @@ namespace SerialPlotAccel
         Series sax = new Series("ax");
         Series say = new Series("ay");
         Series saz = new Series("az");
-        Series sat = new Series("at");
+
+        Series ssx = new Series("sx");
+        Series ssy = new Series("sy");
+        Series ssz = new Series("sz");
+
+        Series spx = new Series("px");
+        Series spy = new Series("py");
+        Series spz = new Series("pz");
 
         private void Form1_Load(object sender, EventArgs e)
         {
             sax.ChartType = SeriesChartType.Line;
             say.ChartType = SeriesChartType.Line;
             saz.ChartType = SeriesChartType.Line;
-            sat.ChartType = SeriesChartType.Line;
 
-            chart1.ChartAreas[0].AxisY.Maximum = 3;
-            chart1.ChartAreas[0].AxisY.Minimum = -3;
-            chart1.Series.Add(sax);
-            chart1.Series.Add(say);
-            chart1.Series.Add(saz);
-            chart1.Series.Add(sat);
+            ssx.ChartType = SeriesChartType.Line;
+            ssy.ChartType = SeriesChartType.Line;
+            ssz.ChartType = SeriesChartType.Line;
+
+            spx.ChartType = SeriesChartType.Line;
+            spy.ChartType = SeriesChartType.Line;
+            spz.ChartType = SeriesChartType.Line;
+
+            //chart_acc.ChartAreas[0].AxisY.Maximum = 3;
+            //chart_acc.ChartAreas[0].AxisY.Minimum = -3;
+            chart_acc.Series.Add(sax);
+            chart_acc.Series.Add(say);
+            chart_acc.Series.Add(saz);
+
+            chart_speed.Series.Add(ssx);
+            chart_speed.Series.Add(ssy);
+            chart_speed.Series.Add(ssz);
+
+            chart_position.Series.Add(spx);
+            chart_position.Series.Add(spy);
+            chart_position.Series.Add(spz);
+
+
+
             this.serialPort1.PortName = "COM3";
             this.serialPort1.Open();
             //while (true) {
@@ -45,10 +69,19 @@ namespace SerialPlotAccel
             //}
         }
 
+        double[] gravity=new double[3];
+        double[] linear_acceleration=new double[3];
+        double[] linear_speed = new double[3];
+        double[] linear_speed_filter = new double[3];
+        double[] linear_position = new double[3];
+        double[] linear_acc_deadzone_count = new double[3];
+
+        int samples = 0;
         private void timer1_Tick(object sender, EventArgs e)
         {
             try
             {
+                samples++;
                 
                 this.serialPort1.WriteLine("g");
 
@@ -61,18 +94,75 @@ namespace SerialPlotAccel
                     double az = double.Parse(aa[2], CultureInfo.InvariantCulture);
                     double at = Math.Sqrt(ax * ax + ay * ay + az * az);
 
+                    // alpha is calculated as t / (t + dT)
+                    // with t, the low-pass filter's time-constant
+                    // and dT, the event delivery rate
 
-                    while (sax.Points.Count > 100) sax.Points.RemoveAt(0);
-                    sax.Points.AddY(ax);
+                    double dt = 5;
+                    double alpha = 1/(1+dt);
+                    alpha = 0.8;
 
-                    while (say.Points.Count > 100) say.Points.RemoveAt(0);
-                    say.Points.AddY(ay);
+                    // Isolate the force of gravity with the low-pass filter.
+                    gravity[0] = alpha * gravity[0] + (1 - alpha) * ax;
+                    gravity[1] = alpha * gravity[1] + (1 - alpha) * ay;
+                    gravity[2] = alpha * gravity[2] + (1 - alpha) * az;
 
-                    while (saz.Points.Count > 100) saz.Points.RemoveAt(0);
-                    saz.Points.AddY(az);
+                    // Remove the gravity contribution with the high-pass filter.
+                    linear_acceleration[0] = ax - gravity[0];
+                    linear_acceleration[1] = ay - gravity[1];
+                    linear_acceleration[2] = az - gravity[2];
 
-                    while (sat.Points.Count > 100) sat.Points.RemoveAt(0);
-                    sat.Points.AddY(at);
+                    //linear accelaretion deadzone
+                    double th=0.025;
+                    if (linear_acceleration[0] > -th && linear_acceleration[0] < th) linear_acceleration[0] = 0;
+                    if (linear_acceleration[1] > -th && linear_acceleration[1] < th) linear_acceleration[1] = 0;
+                    if (linear_acceleration[2] > -th && linear_acceleration[2] < th) linear_acceleration[2] = 0;
+
+                    //deadzone counter
+                    if (linear_acceleration[0] == 0) { linear_acc_deadzone_count[0]++; } else { linear_acc_deadzone_count[0] = 0; }
+                    if (linear_acceleration[1] == 0) { linear_acc_deadzone_count[1]++; } else { linear_acc_deadzone_count[1] = 0; }
+                    if (linear_acceleration[2] == 0) { linear_acc_deadzone_count[2]++; } else { linear_acc_deadzone_count[2] = 0; }
+
+                    if (samples > 30)
+                    {
+                        //
+                        linear_speed[0] += linear_acceleration[0];
+                        linear_speed[1] += linear_acceleration[1];
+                        linear_speed[2] += linear_acceleration[2];
+                        //
+                        int reset_speed = 10;
+                        if (linear_acc_deadzone_count[0] > reset_speed) linear_speed[0] = 0;
+                        if (linear_acc_deadzone_count[1] > reset_speed) linear_speed[1] = 0;
+                        if (linear_acc_deadzone_count[2] > reset_speed) linear_speed[2] = 0;
+
+                        //
+                        linear_position[0] += linear_speed[0];
+                        linear_position[1] += linear_speed[1];
+                        linear_position[2] += linear_speed[2];
+                    }
+
+                    while (sax.Points.Count > 100) sax.Points.RemoveAt(0); sax.Points.AddY(linear_acceleration[0]);
+                    while (say.Points.Count > 100) say.Points.RemoveAt(0); say.Points.AddY(linear_acceleration[1]);
+                    while (saz.Points.Count > 100) saz.Points.RemoveAt(0); saz.Points.AddY(linear_acceleration[2]);
+
+                    while (ssx.Points.Count > 100) ssx.Points.RemoveAt(0); ssx.Points.AddY(linear_speed[0]);
+                    while (ssy.Points.Count > 100) ssy.Points.RemoveAt(0); ssy.Points.AddY(linear_speed[1]);
+                    while (ssz.Points.Count > 100) ssz.Points.RemoveAt(0); ssz.Points.AddY(linear_speed[2]);
+
+                    while (spx.Points.Count > 100) spx.Points.RemoveAt(0); spx.Points.AddY(linear_position[0]);
+                    while (spy.Points.Count > 100) spy.Points.RemoveAt(0); spy.Points.AddY(linear_position[1]);
+                    while (spz.Points.Count > 100) spz.Points.RemoveAt(0); spz.Points.AddY(linear_position[2]);
+
+
+
+                    chart_acc.ChartAreas[0].RecalculateAxesScale();
+                    chart_speed.ChartAreas[0].RecalculateAxesScale();
+                    chart_position.ChartAreas[0].RecalculateAxesScale();
+
+                    //while (sat.Points.Count > 100) sat.Points.RemoveAt(0);
+                    //sat.Points.AddY(at);
+
+
 
                 }
             }
