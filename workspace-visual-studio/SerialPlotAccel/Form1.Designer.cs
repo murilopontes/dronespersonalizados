3df713565c19be149a29f84cf349f09dc1697aa9
murix <murilo.pontes@gmail.com>
2014-01-31 01:35:51 -0300

    beta accelerometer 3d simulator

--- workspace-visual-studio/SerialPlotAccel/Form1.Designer.cs
+++ workspace-visual-studio/SerialPlotAccel/Form1.Designer.cs
@@ -29,20 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.chart_acc = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart_speed = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart_position = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart_acc)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_speed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_position)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -57,45 +60,61 @@
             // 
             // chart_acc
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart_acc.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart_acc.Legends.Add(legend1);
+            chartArea7.Name = "ChartArea1";
+            this.chart_acc.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart_acc.Legends.Add(legend7);
             this.chart_acc.Location = new System.Drawing.Point(33, 30);
             this.chart_acc.Name = "chart_acc";
-            this.chart_acc.Size = new System.Drawing.Size(629, 136);
+            this.chart_acc.Size = new System.Drawing.Size(494, 136);
             this.chart_acc.TabIndex = 0;
             this.chart_acc.Text = "chart_acc";
             // 
             // chart_speed
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart_speed.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart_speed.Legends.Add(legend2);
+            chartArea8.Name = "ChartArea1";
+            this.chart_speed.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart_speed.Legends.Add(legend8);
             this.chart_speed.Location = new System.Drawing.Point(33, 172);
             this.chart_speed.Name = "chart_speed";
-            this.chart_speed.Size = new System.Drawing.Size(629, 136);
+            this.chart_speed.Size = new System.Drawing.Size(494, 136);
             this.chart_speed.TabIndex = 1;
             this.chart_speed.Text = "chart_acc";
             // 
             // chart_position
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart_position.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart_position.Legends.Add(legend3);
+            chartArea9.Name = "ChartArea1";
+            this.chart_position.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chart_position.Legends.Add(legend9);
             this.chart_position.Location = new System.Drawing.Point(33, 314);
             this.chart_position.Name = "chart_position";
-            this.chart_position.Size = new System.Drawing.Size(629, 136);
+            this.chart_position.Size = new System.Drawing.Size(494, 136);
             this.chart_position.TabIndex = 2;
             this.chart_position.Text = "chart_acc";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(533, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 420);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 496);
+            this.ClientSize = new System.Drawing.Size(953, 496);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.chart_position);
             this.Controls.Add(this.chart_speed);
             this.Controls.Add(this.chart_acc);
@@ -105,6 +124,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart_acc)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_speed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_position)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -116,6 +136,8 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_acc;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_speed;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_position;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- workspace-visual-studio/SerialPlotAccel/Form1.cs
+++ workspace-visual-studio/SerialPlotAccel/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Axiom.Core;
+using Axiom.Graphics;
+using Axiom.Math;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -32,8 +35,158 @@ namespace SerialPlotAccel
         Series spy = new Series("py");
         Series spz = new Series("pz");
 
+
+
+
+
+
+
+        public void OnLoad()
+        {
+            ResourceGroupManager.Instance.AddResourceLocation("media", "Folder", true);
+
+            _scene = _engine.CreateSceneManager("DefaultSceneManager", "DefaultSM");
+            _scene.ClearScene();
+
+            //_scene = Root.Instance.CreateSceneManager(SceneType.ExteriorClose);
+            //_scene.LoadWorldGeometry("Terrain.xml");
+
+
+            _camera = _scene.CreateCamera("MainCamera");
+
+            //
+            _camera.Position = new Vector3(0, 170, 300); // x=right/left from screen,y=bottom/up from screen,z=in/out from scren
+            _camera.LookAt(Vector3.Zero);
+
+            //The clipping distance of a Camera specifies how close or far something can be before you no longer see it. 
+            _camera.Near = 5;
+
+
+            _camera.AutoAspectRatio = true;
+
+            var vp = _window.AddViewport(_camera, 0, 0, 1.0f, 1.0f, 100);
+            vp.BackgroundColor = ColorEx.CornflowerBlue;
+
+            ResourceGroupManager.Instance.InitializeAllResourceGroups();
+
+        }
+
+        SceneNode node = null;
+
+        public void CreateScene()
+        {
+
+            //shadows and ambient light
+            _scene.AmbientLight = ColorEx.Black;
+
+            //_scene.ShadowTechnique = ShadowTechnique.StencilAdditive;
+            _scene.ShadowTechnique = ShadowTechnique.StencilModulative;
+            //_scene.ShadowTechnique = ShadowTechnique.TextureAdditive;
+            //_scene.ShadowTechnique = ShadowTechnique.TextureModulative;
+
+
+            Entity ent = _scene.CreateEntity("Penguin", "penguin.mesh");
+            ent.CastShadows = true;
+
+            node = _scene.RootSceneNode.CreateChildSceneNode("PenguinNode");
+            node.AttachObject(ent);
+            node.Position += new Vector3(25, 0, 0);
+            //node.Scale = new Vector3(0.5f, 0.5f, 0.5f);
+            //node2.ScaleBy(new Vector3(10, 10, 10));
+            node.Yaw(180);
+            node.Pitch(30);
+            node.Roll(30);
+            node.Position += new Vector3(0, 50, 0);
+            node.Orientation = Quaternion.FromEulerAngles(0, 0, 0);
+
+            Entity ent2 = _scene.CreateEntity("Penguin2", "penguin.mesh");
+            //ent2.IsVisible = false;
+            //ent2.Name;
+            //ent2.ParentSceneNode;
+            ent2.CastShadows = true;
+            SceneNode node2 = _scene.RootSceneNode.CreateChildSceneNode("Penguin2", new Vector3(0, 0, 0));
+            node2.AttachObject(ent2);
+            node2.Position += new Vector3(-50, 25, -50);
+
+            //create ground
+            Plane plane = new Plane(Vector3.UnitY, 0);
+            MeshManager.Instance.CreatePlane("ground", ResourceGroupManager.DefaultResourceGroupName, plane, 300, 300, 20, 20, true, 1, 5, 5, Vector3.UnitZ);
+            Entity groundEnt = _scene.CreateEntity("GroundEntity", "ground");
+            _scene.RootSceneNode.CreateChildSceneNode().AttachObject(groundEnt);
+            groundEnt.CastShadows = false;
+            groundEnt.MaterialName = "Rockwall";
+
+            //create light 
+            Light pointLight = _scene.CreateLight("pointLight");
+            pointLight.Type = LightType.Point;
+            pointLight.Position = new Vector3(0, 200, 0);
+            pointLight.Diffuse = ColorEx.White;
+            pointLight.Specular = ColorEx.White;
+
+        }
+
+
+        public void OnUnload()
+        {
+        }
+
+
+        void HandleKeyboardInput(FrameEventArgs e)
+        {
+            //InputReader input = new InputReader();
+            //input.Capture();
+        }
+
+
+        public void OnRenderFrame(object s, FrameEventArgs e)
+        {
+            /*
+            Gamepad_State_SlimDX joy = new Gamepad_State_SlimDX(SlimDX.XInput.UserIndex.One);
+            joy.Update();
+
+            node.Pitch(joy.LeftStick.Position.Y);
+            node.Roll(joy.LeftStick.Position.X);
+            node.Yaw(joy.RightStick.Position.X);
+
+
+            node.Position += new Vector3(0, joy.RightStick.Position.Y, 0);
+
+            if (node.Position.y < 25)
+            {
+                node.Position = new Axiom.Math.Vector3(node.Position.x, 25, node.Position.z);
+            }
+
+            HandleKeyboardInput(e);
+            */
+        }
+
+
+        private Root _engine;
+        private RenderWindow _window;
+        private SceneManager _scene;
+        private Camera _camera;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US", false);
+            
+            _engine = new Root("Game.log");
+            _engine.RenderSystem = _engine.RenderSystems[0];
+            
+            
+            _window = _engine.Initialize(true);
+            
+            /*
+            OnLoad();
+            CreateScene();
+            _engine.FrameRenderingQueued += OnRenderFrame;
+            _engine.StartRendering();
+            OnUnload();
+            */
+
+
+
+
             sax.ChartType = SeriesChartType.Line;
             say.ChartType = SeriesChartType.Line;
             saz.ChartType = SeriesChartType.Line;
@@ -169,6 +322,12 @@ namespace SerialPlotAccel
             catch (Exception ex) { 
             
             }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+  
 
         }
     }
