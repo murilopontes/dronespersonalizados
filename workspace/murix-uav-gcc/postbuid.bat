78021f5abfa28002c8d4dab46fb832896b537b0a
murix <murilo.pontes@gmail.com>
2014-03-19 00:55:34 +0000

    cleanup, port to archlinux

--- workspace/murix-uav-gcc/postbuid.bat
+++ workspace/murix-uav-gcc/postbuid.bat
@@ -7,16 +7,6 @@ arm-none-eabi-objcopy -O binary murix-uav-gcc.elf murix-uav-gcc.bin
 arm-none-eabi-objcopy -O ihex   murix-uav-gcc.elf murix-uav-gcc.hex
 
 @echo "print size"
-@echo "---------------------------------------"
-@echo "text - shows the code and read-only data in your application (in decimal)"
-@echo "data - shows the read-write data in your application (in decimal)"
-@echo "bss - show the zero initialized ('bss' and 'common') data in your application (in decimal)"
-@echo "dec - total of 'text' + 'data' + 'bss' (in decimal)"
-@echo "hex - hexidecimal equivalent of 'dec'"
-@echo "---------------------------------------"
-@echo "the flash consumption of your application will then be text + data"
-@echo "the RAM consumption of your application will then be data + bss."
-@echo "---------------------------------------"
 arm-none-eabi-size              murix-uav-gcc.elf
 
 @echo "create listing"
