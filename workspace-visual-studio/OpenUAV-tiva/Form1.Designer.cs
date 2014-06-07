5bc427c233cfeb897ecc0e99ee78c285112f1c67
murix <murilo.pontes@gmail.com>
2014-06-07 13:45:58 -0300

    xbox controller RF serial send

--- workspace-visual-studio/OpenUAV-tiva/Form1.Designer.cs
+++ workspace-visual-studio/OpenUAV-tiva/Form1.Designer.cs
@@ -30,23 +30,80 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "open serial";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "close serial";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(283, 108);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 261);
+            this.ClientSize = new System.Drawing.Size(305, 160);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Xbox to RF serial";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- workspace-visual-studio/OpenUAV-tiva/Form1.cs
+++ workspace-visual-studio/OpenUAV-tiva/Form1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using murix_utils;
+using SlimDX;
+using SlimDX.XInput;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +22,137 @@ namespace DroneMURIX
             InitializeComponent();
         }
 
+
+        public class TextBoxStreamWriter : TextWriter
+        {
+            TextBox _output = null;
+
+            public TextBoxStreamWriter(TextBox output)
+            {
+                _output = output;
+            }
+
+            public override void Write(char value)
+            {
+                //cross thread
+                MethodInvoker action = delegate { _output.AppendText(value.ToString()); };
+                _output.BeginInvoke(action);
+            }
+
+            public override Encoding Encoding
+            {
+                get { return System.Text.Encoding.UTF8; }
+            }
+        }
+
+        // That's our custom TextWriter class
+        TextWriter _writer = null;
+
+        Gamepad_State_SlimDX joy = new Gamepad_State_SlimDX(UserIndex.One);
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.serialPort1.PortName = "COM4";
-            this.serialPort1.BaudRate = 115200;
-            this.serialPort1.Open();
+            // Instantiate the writer
+            _writer = new TextBoxStreamWriter(this.textBox1);
+            // Redirect the out Console stream
+            Console.SetOut(_writer);
+            Console.WriteLine("Now redirecting output to the text box");
+
+            this.comboBox1.Items.AddRange(SerialPort.GetPortNames());
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            if(this.comboBox1.Items.Count>0) this.comboBox1.SelectedIndex = 0;
+
+            serialPort1.BaudRate = 115200;
+            serialPort1.DataReceived += serialPort1_DataReceived;
+            serialPort1.ReadTimeout = 50;
+            timer1.Start();
         }
+
+        void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Console.Write(serialPort1.ReadExisting());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen) serialPort1.Close();
+                serialPort1.PortName = (string)this.comboBox1.SelectedItem;
+                serialPort1.Open();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            joy.Update();
+            if (serialPort1.IsOpen) {
+
+                //
+                int yaw = (int)(joy.LeftStick.Position.X * 32768);
+                int throttle = (int)(joy.LeftStick.Position.Y * 32768);
+                bool ls = joy.LeftStick.Clicked;
+                bool lb=joy.LeftShoulder;
+                float lt = joy.LeftTrigger;
+
+                //
+                bool up = joy.DPad.Up;
+                bool down = joy.DPad.Down;
+                bool right = joy.DPad.Right;
+                bool left = joy.DPad.Left;
+
+                //
+                int roll = (int)(joy.RightStick.Position.X * 32768);
+                int pitch = (int)(joy.RightStick.Position.Y * 32768);
+                bool rs = joy.RightStick.Clicked;
+                bool rb = joy.RightShoulder;
+                float rt = joy.RightTrigger;
+
+                //
+                bool start = joy.Start;
+                bool back = joy.Back;
+
+                //                
+                bool a=joy.A;
+                bool b=joy.B;
+                bool x = joy.X;
+                bool y = joy.Y;
+
+                //
+
+
+
+
+                string saida = "joy(" + throttle + "," + roll +"," + pitch + "," + yaw + ")\r\n";
+                try
+                {
+                    serialPort1.Write(saida);
+                    string lido = serialPort1.ReadTo("ok");
+                    Console.WriteLine(lido);
+                }
+                catch (Exception ex) { 
+                    
+                }
+            }
+        }
+
+
+
     }
 }
